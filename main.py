89cd8752f7bb5db6f886114f358c3543bff24ca3
dekapaan <terblancheronald@gmail.com>
2021-04-13 09:36:34 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+_pi = 3.14  # if constant, start var with "_"
+r = float(input('Enter Radius of circle (cm): '))  # radius input
+area, diameter, circ = _pi*(r ** 2), 2*r, 2*_pi*r  # Formulae for area, diameter and circumference
+print("")
+print("Area of circle is", area, "cm^2")
+print("Diameter of circle is", diameter, "cm")
+print("Circumference of circle is", circ, "cm")
+
+
+
